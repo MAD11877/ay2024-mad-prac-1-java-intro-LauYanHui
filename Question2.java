c1ecb1e4023416213e403aaa6bc33bb4a6e828a7
YanHui <150310203+LauYanHui@users.noreply.github.com>
2024-04-15 15:29:20 +0800

    .

--- Question2.java
+++ Question2.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+// import org.junit.jupiter.api.Test;
+import java.util.Scanner; // Import Scanner for reading input
+
+public class Question2 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter weight: ");
+    double weight = scanner.nextDouble();
+    System.out.println("Weight entered: " + weight + "Kg");
+    System.out.print("Enter Height: ");
+    double height = scanner.nextDouble();
+    System.out.println("Height Entered" + height + " cm");
+    double BMI = weight / ((height/100) * (height/100));
+    System.out.println("BMI is "+ BMI);
   }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
 }
--- Question3.java
+++ Question3.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
-public class Question3
-{
-  public static void main(String[] args)
-  {
+public class Question3 {
+  public static void main(String[] args) {
     /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
+     * Reads integer entered by the user, multiplies the integer by itself and print
+     * out the result.
      * 
      * Hint: in.nextInt() to read integer
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter Number: ");
+    int num = in.nextInt();
+    int num1 = num * num;
+    System.out.println("Number is: " + num1);
+
   }
 }
